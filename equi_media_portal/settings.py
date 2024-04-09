4f5a065dcaf26c86711a28da942bd9eab15af3ff
ipsorus <ipsorus@inbox.ru>
2024-04-09 11:24:47 +0300

    1

--- equi_media_portal/settings.py
+++ equi_media_portal/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'equi_media_portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'equimedia',
+        'USER': 'admin',
+        'PASSWORD': 'zxcv0987',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
