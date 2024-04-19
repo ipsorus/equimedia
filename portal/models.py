367c59f00405cde8dc96d01d095b090b56e08c1f
vloshkarev@it-one.ru <vloshkarev@it-one.ru>
2024-04-19 14:22:02 +0300

    1

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,21 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, db_index=True, verbose_name="Заголовок новости")
+    content = RichTextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False, verbose_name="Публикация записи")
+    category = models.ManyToManyField('Category', default=1, related_name='posts', verbose_name="Категории")
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=30, db_index=True, verbose_name="Название категории")
+
+    def __str__(self):
+        return f'{self.title}'
